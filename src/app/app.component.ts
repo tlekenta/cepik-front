f9fb3b0590b70811e267fd1332d78aca2f95cf13
Tomasz Lekenta <lekenta95@gmail.com>
2017-11-08 15:47:18 +0100

    Fix bledow Marka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import {Router} from '@angular/router';
 })
 export class AppComponent {
   constructor(private router: Router) {
-    this.router.navigate(['/home']);
+    this.router.navigate(['/dashboard']);
   }
 }
--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LayoutComponent} from './layout.component';
-import { BlankPageComponent } from './pages/blank-page/blank-page.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { VehiclePageComponent} from './pages/vehicle-page/vehicle-page.component';
 
 const routes: Routes = [
   { path: '', component: LayoutComponent,
     children: [
-      { path: '', component: BlankPageComponent },
       { path: 'dashboard', component: DashboardComponent },
       { path: 'vehicle', component: VehiclePageComponent }
     ]
