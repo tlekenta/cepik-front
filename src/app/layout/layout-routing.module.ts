f2feeaba86ebaafef6ccf4c97da7f16c61476cfa
Tomasz Lekenta <lekenta95@gmail.com>
2017-11-11 01:39:33 +0100

    Fix moich błędów.
    Generalnie ogarnięty dostęp.
    Metodą prób i błędów, ale na razie działa tak jak powinien.

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -4,13 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent} from './layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { VehiclePageComponent} from './pages/vehicle-page/vehicle-page.component';
+import {AuthGuardService} from '../services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', component: LayoutComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
-      { path: 'vehicle', component: VehiclePageComponent }
-    ]
+      { path: 'vehicle', component: VehiclePageComponent },
+      { path: '', component: DashboardComponent }
+    ],
+    canActivate: [AuthGuardService]
   }
 ];
 
