3cd5805df91731b4f12dd658eb29b6f6fff38b64
Tomasz Lekenta <lekenta95@gmail.com>
2018-01-22 20:35:04 +0100

    Wyrzucenie zbędnego moim zdaniem modułu

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,16 +5,16 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LayoutComponent } from './layout.component';
 import { PagesModule } from './pages/pages.module';
-import { NavbarModule } from './navbar/navbar.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserPanelComponent } from './navbar/user-panel/user-panel.component';
 
-const components = [SidebarComponent, LayoutComponent];
+const components = [SidebarComponent, LayoutComponent, NavbarComponent, UserPanelComponent];
 
 @NgModule({
   imports: [
     CommonModule,
     PagesModule,
     LayoutRoutingModule,
-    NavbarModule
   ],
   declarations: components,
   exports: components
