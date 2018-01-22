ff3a3c649b51cb95df443488c81fcaf49df122a0
Tomasz Lekenta <lekenta95@gmail.com>
2018-01-22 23:55:46 +0100

    Pierwsza wersja tabelki dla samochodów
    Poprawy w css'ach layoutu

--- src/app/layout/pages/vehicle-page/vehicle-page.component.ts
+++ src/app/layout/pages/vehicle-page/vehicle-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehicle } from '../../../model/vehicle';
 
 @Component({
   selector: 'app-vehicle-page',
   templateUrl: './vehicle-page.component.html',
-  styleUrls: ['./vehicle-page.component.css']
+  styleUrls: ['./vehicle-page.component.scss']
 })
 export class VehiclePageComponent implements OnInit {
   title = "Vehicle";
+
+  table: Array<Vehicle> = [];
   constructor() { }
 
   ngOnInit() {
+      this.table.push({id: 1, numer_rejestracyjny: 'WPR 89TM', marka: 'Opel', model: 'Corsa', pojemnnosc_silnika: 1.2});
+      this.table.push({id: 1, numer_rejestracyjny: 'WPR 89TM', marka: 'Opel', model: 'Corsa', pojemnnosc_silnika: 1.2});
+      this.table.push({id: 1, numer_rejestracyjny: 'WPR 89TM', marka: 'Opel', model: 'Corsa', pojemnnosc_silnika: 1.2});
+      this.table.push({id: 1, numer_rejestracyjny: 'WPR 89TM', marka: 'Opel', model: 'Corsa', pojemnnosc_silnika: 1.2});
+      this.table.push({id: 1, numer_rejestracyjny: 'WPR 89TM', marka: 'Opel', model: 'Corsa', pojemnnosc_silnika: 1.2});
+      this.table.push({id: 1, numer_rejestracyjny: 'WPR 89TM', marka: 'Opel', model: 'Corsa', pojemnnosc_silnika: 1.2});
+      this.table.push({id: 1, numer_rejestracyjny: 'WPR 89TM', marka: 'Opel', model: 'Corsa', pojemnnosc_silnika: 1.2});
+      this.table.push({id: 1, numer_rejestracyjny: 'WPR 89TM', marka: 'Opel', model: 'Corsa', pojemnnosc_silnika: 1.2});
   }
 
 }
---
+++ src/app/model/vehicle.ts
@@ -0,0 +1,8 @@
+export class Vehicle {
+    id: number;
+    numer_rejestracyjny: string;
+    marka: string;
+    model: string;
+    pojemnnosc_silnika: number;
+    // itd
+}
