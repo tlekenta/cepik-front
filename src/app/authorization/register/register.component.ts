ec7ea1f2abcdcc4975f048ac4193aaf8df562311
Tomasz Lekenta <lekenta95@gmail.com>
2018-01-22 21:17:22 +0100

    css'y rejestracji

--- src/app/authorization/register/register.component.ts
+++ src/app/authorization/register/register.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
   passwordRequiments = '- minimalna liczba znaków: 6\n- jedna wielka litera';
