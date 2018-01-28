c3bba7f081b112cd63b77c7f269cc59b5521923f
Tomasz Lekenta <lekenta95@gmail.com>
2018-01-28 03:53:16 +0100

    Dodawanie kierowcy

--- src/app/layout/pages/driver-page/driver-add/driver-add.component.ts
+++ src/app/layout/pages/driver-page/driver-add/driver-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DriverForm } from '../../../../model/cek/form/driver-form';
+import { DriverService } from '../../../../services/cek/driver.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-driver-add',
@@ -13,13 +15,26 @@ export class DriverAddComponent implements OnInit {
   private errorMsg = "";
   private successMsg = "Poprawnie dodano kierowcę";
 
-  constructor() { }
+  constructor(private service: DriverService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
   add(){
     console.log(this.driver);
+    this.service.saveDriver(this.driver)
+      .toPromise()
+      .then(resp => {
+        this.error = false;
+        this.success  = true;
+        this.router.navigate(["/driver/" + resp.id]);
+      })
+      .catch(error => {
+        this.errorMsg = error.error;
+        this.error = true;
+        this.success = false;
+      });
   }
 
 }
